4ea036b2f2bf5c5aada32f1de275d98b5227712b
Saif Ullah <saif.ullah@fuelbuddy.in>
2025-07-03 11:59:03 +0530

    changed mail to and tel

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,29 +1,33 @@
-import { ArrowDown } from "@phosphor-icons/react"
-import zigzagUnderline from "../assets/zigzag.svg"
+import { ArrowDown } from "@phosphor-icons/react";
+import zigzagUnderline from "../assets/zigzag.svg";
 
-import mailIcon from "../assets/contact-icons/mail.svg"
-import phoneIcon from "../assets/contact-icons/phone.svg"
-import locationIcon from "../assets/contact-icons/location.svg"
-import CTABanner from "../common/CTABanner"
-import Map from "../components/Map"
-import FAQ from "../components/FAQ"
+import mailIcon from "../assets/contact-icons/mail.svg";
+import phoneIcon from "../assets/contact-icons/phone.svg";
+import locationIcon from "../assets/contact-icons/location.svg";
+import CTABanner from "../common/CTABanner";
+import Map from "../components/Map";
+import FAQ from "../components/FAQ";
 
 const Contact = () => {
   return (
     <div className="overflow-hidden">
       <div className="pt-20 bg-sidekick-green">
         <div className="px-6 md:px-0 py-16 flex items-center justify-between max-w-7xl mx-auto md:w-[80vw]">
-          <h2 className="text-3xl font-bold md:text-6xl md:font-semibold text-gray-800 tracking-tight">Contact Us</h2>
-          <ArrowDown size={36} className="text-white bg-sidekick-blue w-20 h-20 p-4 rounded-full" />
+          <h2 className="text-3xl font-bold md:text-6xl md:font-semibold text-gray-800 tracking-tight">
+            Contact Us
+          </h2>
+          <ArrowDown
+            size={36}
+            className="text-white bg-sidekick-blue w-20 h-20 p-4 rounded-full"
+          />
         </div>
       </div>
-
-
 
       {/* Blue Section text */}
       <section className=" space-y-6 md:space-y-12 relative max-w-7xl mx-auto  py-12 px-6 md:px-0">
         <h3 className="text-sidekick-blue text-xl py-3 md:text-3xl  md:py-6 text-center tracking-wide">
-          We know you move fast,<br />
+          We know you move fast,
+          <br />
           and so should your support.
         </h3>
 
@@ -31,30 +35,51 @@ const Contact = () => {
           <h2 className="text-center text-3xl md:text-4xl font-bold z-10">
             Talk to Us
           </h2>
-          <img src={zigzagUnderline} alt="" aria-hidden className="hidden md:block relative select-none ml-auto left-6 bottom-3" />
+          <img
+            src={zigzagUnderline}
+            alt=""
+            aria-hidden
+            className="hidden md:block relative select-none ml-auto left-6 bottom-3"
+          />
         </div>
 
         <div className="flex flex-col gap-8 py-4 md:py-0 items-center justify-between md:grid md:grid-cols-3 md:gap-2 place-items-center max-w-5xl mx-auto mb-12">
           <div>
             <img src={mailIcon} alt="mail icon" className="mx-auto mb-4" />
-            <a href="mailto:support@sidekick.in" className="block mb-1 text-lg font-semibold">
-              support@sidekick.in
+            <a
+              href="mailto:admin@gosidekick.in"
+              className="block mb-1 text-lg font-semibold"
+            >
+              admin@gosidekick.in
             </a>
-            <p className="text-center text-sm md:font-normal font-medium text-gray-500">Email</p>
+            <p className="text-center text-sm md:font-normal font-medium text-gray-500">
+              Email
+            </p>
           </div>
           <div>
             <img src={phoneIcon} alt="phone icon" className="mx-auto mb-4" />
-            <a href="tel:+91 9538591731" className="block mb-1 text-lg font-semibold">
-              +91 9538591731
+            <a
+              href="tel:+91 8595599661"
+              className="block mb-1 text-lg font-semibold"
+            >
+              +91 8595599661
             </a>
-            <p className="text-center text-sm md:font-normal font-medium text-gray-500">Call/WhatsApp</p>
+            <p className="text-center text-sm md:font-normal font-medium text-gray-500">
+              Call/WhatsApp
+            </p>
           </div>
           <div>
-            <img src={locationIcon} alt="location icon" className="mx-auto mb-4" />
+            <img
+              src={locationIcon}
+              alt="location icon"
+              className="mx-auto mb-4"
+            />
             <p className="block mb-1 text-lg font-semibold">
               Find one near you
             </p>
-            <p className="text-center text-sm md:font-normal font-medium text-gray-500">Service Centers</p>
+            <p className="text-center text-sm md:font-normal font-medium text-gray-500">
+              Service Centers
+            </p>
           </div>
         </div>
       </section>
@@ -70,8 +95,10 @@ const Contact = () => {
               Live chat with our team for real-time assistance.
             </p>
           </div>
-          <a href="#"
-            className="bg-white rounded-full px-5 py-2.5 block w-max ml-auto font-medium">
+          <a
+            href="#"
+            className="bg-white rounded-full px-5 py-2.5 block w-max ml-auto font-medium"
+          >
             Whatsapp
           </a>
         </div>
@@ -82,7 +109,7 @@ const Contact = () => {
       <FAQ />
       <CTABanner />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
